0e063c52fdcde251c791667d373da37e2b34973c
valen <nicolas.andrada@hotmail.com>
2023-09-22 15:16:30 -0300

    a

--- peliculas/guardar.php
+++ peliculas/guardar.php
@@ -5,7 +5,7 @@ $foto = $_FILES['foto'];
 
 var_dump($foto);
 
-move_uploaded_file($foto['tmp_name'], '../img/$foto[name]')
+move_uploaded_file($foto['tmp_name'], '../img/$foto[name]');
 
 $sql = "INSERT INTO pelicula(titulo, foto, genero)
         VALUES ('pinocho', 'pino.jpg', 'comedia, drama')";
